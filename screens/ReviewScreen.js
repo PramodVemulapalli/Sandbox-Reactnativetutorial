7c39aea9efd0884f56934b9ef8923a2e62f35368
Pramod Vemulapalli <v.pramodkrishna@gmail.com>
2017-08-20 17:14:52 -0400

    added button to review screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
   static navigationOptions = {
       title: 'Review Jobs',
-      headerRight: <Text>Go Right</Text>
+      headerRight: <Button title="Settings" onPress={() => } />
     };
-
-
+    
   render() {
     return (
       <View>
